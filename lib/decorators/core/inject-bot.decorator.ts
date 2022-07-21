97ce497b3cb4924e1e919cc5e4c6fd385f2f5759
xTCry [Vladislav Kh] <wolt6@mail.ru>
2022-07-21 23:43:58 +0300

    fix(types): allow injection outside constructor parameter

--- lib/decorators/core/inject-bot.decorator.ts
+++ lib/decorators/core/inject-bot.decorator.ts
@@ -1,5 +1,5 @@
 import { Inject } from '@nestjs/common';
 import { getBotToken } from '../../utils';
 
-export const InjectBot = (botName?: string): ParameterDecorator =>
+export const InjectBot = (botName?: string): ReturnType<typeof Inject> =>
   Inject(getBotToken(botName));
